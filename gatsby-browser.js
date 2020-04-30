df4567767d7949618b35bb182437765468e30ef5
Nahiyan Kamal <noyanayihan@gmail.com>
2020-04-30 03:28:08 +0200

    auto generated message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,11 @@
 // this is to install code syntax highlighting
 import "./node_modules/highlight.js/styles/railscasts.css";
+import "./src/css/layout.css";
+
+import React from "react";
+import ThemeProvider from "./src/Context/ThemeProvider";
+
+// this is a gatsby function
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider>{element}</ThemeProvider>;
+};
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./Layout/Header/Header";
 import Footer from "./Layout/Footer/Footer";
 
-import "../css/layout.css";
-import ThemeContext, { darkTheme } from "../Context/ThemeContext";
+import ThemeContext from "../Context/ThemeContext";
 import { TopMostDivWrapper } from "../css/commonstyles";
 
 const Layout = ({ children }) => {
-  const [theme, setTheme] = useState(darkTheme);
-  const value = { theme, setTheme };
   return (
-    <ThemeContext.Provider value={value}>
-      <TopMostDivWrapper theme={theme}>
-        <Header />
-        {children}
-        <Footer />
-      </TopMostDivWrapper>
-    </ThemeContext.Provider>
+    <ThemeContext.Consumer>
+      {({ theme }) => (
+        <TopMostDivWrapper theme={theme}>
+          <Header />
+          {children}
+          <Footer />
+        </TopMostDivWrapper>
+      )}
+    </ThemeContext.Consumer>
   );
 };
 
