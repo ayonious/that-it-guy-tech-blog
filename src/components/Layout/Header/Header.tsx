f833e682ceef4dfc31c3a2faaad10872f1bf68b2
Nahiyan Kamal <noyanayihan@gmail.com>
2020-05-01 18:53:59 +0200

    auto generated message

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
-import TagSearchBox from "../../TagSearchBox/TagSearchBox";
 import { NavBar } from "./styles";
 
 const Header = () => {
   return (
     <NavBar>
       <ThemeSwitcher />
-      <TagSearchBox />
     </NavBar>
   );
 };
