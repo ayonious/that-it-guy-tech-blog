1acb3eeb91886252c5b555874b7b11596783d5ba
Nahiyan Kamal <noyanayihan@gmail.com>
2020-05-01 16:10:44 +0200

    auto generated message

--- src/components/Layout/TagSearchBar/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const SearchBarDivWrapper = styled.div`
-  width: 100px;
-  background: ${(p) => p.theme.mainGrey};
-  color: ${(p) => p.theme.mainGrey};
-`;
