87d2a0bfed346f67c4e39f585d5aca205a733c31
Nahiyan Kamal <noyanayihan@gmail.com>
2020-05-07 00:29:18 +0200

    auto generated message

--- src/components/ThemedTopMostDivWrapper.tsx
+++ src/components/ThemedTopMostDivWrapper.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { TopMostDivWrapper } from "../css/commonstyles";
 
 const ThemedTopMostDivWrapper = ({ children, theme }) => {
+  const [value, setValue] = useState(theme);
+  useEffect(() => {
+    setValue(value);
+  }, [value]);
+
   return <TopMostDivWrapper theme={theme}>{children}</TopMostDivWrapper>;
 };
 
