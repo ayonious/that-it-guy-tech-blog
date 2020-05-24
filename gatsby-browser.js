1edd650c6e0a8d5738a69e4420307f4c2628345c
Nahiyan Kamal <noyanayihan@gmail.com>
2020-05-24 12:57:59 +0200

    auto generated message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,6 @@
 import "./node_modules/highlight.js/styles/railscasts.css";
 import "./src/css/layout.css";
 
-// clipboard to copy code
-
 import React from "react";
 import ThemeProvider from "./src/Context/ThemeProvider";
 
--- src/components/CodeWrapper/CodeWrapper.tsx
+++ src/components/CodeWrapper/CodeWrapper.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import { CodeDivWrapper, CopyButtonWrapper } from "./styles";
 
-const CopyButton = () => {
+const witeInClipboard = (text) => {
+  navigator.clipboard.writeText(text);
+};
+
+const CopyButton = (props) => {
   return (
-    <CopyButtonWrapper>
+    <CopyButtonWrapper onClick={props.onCopy}>
       <span>copy</span>
     </CopyButtonWrapper>
   );
 };
 
+let isString = (inp) => typeof inp === "string";
+
+const getCodeText = (children) => {
+  let current = children;
+  while (!isString(current)) {
+    current = current.props.children;
+  }
+  return current;
+};
+
 const CodeWrapper = ({ children }) => {
+  // a hacky way to getting the code part
+  const codeText = getCodeText(children);
+
   return (
     <CodeDivWrapper>
-      <CopyButton />
+      <CopyButton onCopy={() => witeInClipboard(codeText)} />
       {children}
     </CodeDivWrapper>
   );
