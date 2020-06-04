b861b55dcc40ba89e727e893245a5f487245dd2e
Nahiyan Kamal <noyanayihan@gmail.com>
2020-06-04 10:35:42 +0200

    auto generated message

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,8 @@ import Layout from "../components/Layout";
 import { Link } from "gatsby";
 import { LinkDivWrapper } from "../css/commonstyles";
 
+import { PostsSectionWrapper } from "../components/PostList/styles";
+
 import ThemeContext from "../Context/ThemeContext";
 
 const Error = () => {
@@ -10,12 +12,14 @@ const Error = () => {
     <ThemeContext.Consumer>
       {({ theme }) => (
         <Layout>
-          <h1>Oops you reached a deadend</h1>
-          <h4>
-            <LinkDivWrapper theme={theme}>
-              <Link to={`/`}>Go Back To Home</Link>
-            </LinkDivWrapper>
-          </h4>
+          <PostsSectionWrapper>
+            <h1>Oops you reached a deadend!!</h1>
+            <h4>
+              <LinkDivWrapper theme={theme}>
+                <Link to={`/`}>Go Back To Home</Link>
+              </LinkDivWrapper>
+            </h4>
+          </PostsSectionWrapper>
         </Layout>
       )}
     </ThemeContext.Consumer>
