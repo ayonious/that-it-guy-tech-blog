c6e0ee08ba0fff42131b5e5ebd52355d4fd38e33
Nahiyan Kamal <noyanayihan@gmail.com>
2021-05-13 22:47:59 +0200

    fix(technical): fixing integration tests

---
+++ cypress/integration/HomePageTest.js
@@ -0,0 +1,36 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:9000");
+  });
+
+  it("Basic Elements are there", () => {
+    cy.contains("That IT Guy");
+    cy.contains("Personal Tech Blog");
+    cy.contains("Filter By Tags");
+    cy.contains("Read More");
+    cy.contains("This is an opensource project you can find the source code in");
+  });
+
+  it("Theme Changes", () => {
+    cy.contains('🌜').click({force: true});
+    cy.contains('🌞').click({force: true});
+    cy.contains('🌜');
+  });
+
+
+  it("Filter By Tags Work", () => {
+    cy.contains('Filter By Tags').click();
+    cy.contains('oop').click();
+    cy.contains('Is Js OOP?');
+  });
+
+  it("Home Page to Blog Post Navigation Works", () => {
+    cy.contains('Filter By Tags').click();
+    cy.contains('oop').click();
+    cy.contains('Is Js OOP?');
+    cy.contains('Read More').click({force: true});
+    cy.contains('But js creates this objects through prototypes.');
+    cy.contains('That IT Guy').click();
+    cy.contains('But js creates this objects through prototypes.').should('not.exist');
+  });
+});
