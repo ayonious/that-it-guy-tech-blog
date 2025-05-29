682a76ce4e845f5d3d6ccd64d49184942a4727f8
Nahiyan Kamal <noyanayihan@gmail.com>
2025-05-29 21:45:36 +0200

    fix: github Action codecov

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   video: false,
   chromeWebSecurity: false,
   e2e: {
+    baseUrl: 'http://localhost:9000/that-it-guy-tech-blog',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
